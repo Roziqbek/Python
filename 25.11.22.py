38cdb5ac653495a8065b892dd0d20e963e0572b0
Roziqbek Donobekovich <117650773+Roziqbek@users.noreply.github.com>
2022-12-08 21:51:51 +0500

    Initial commit

---
+++ 25.11.22.py
@@ -0,0 +1,171 @@
+import math
+1 # def PowerA3(a,b,c,d,e):
+#     return a**3,b**3,c**3,d**3,e**3
+# print(PowerA3(2,3,4,5,6))
+
+2 # def Power234(a,b,c):
+#     return a**2,b**3,c**4
+# print(Power234(1,2,3))
+
+3 # class Son:
+#     def __init__(self,A,B,C,D) -> None:
+#         self.A = A
+#         self.B = B
+#         self.C = C
+#         self.D = D
+#     def arifmetik(self) -> None:
+#         return (self.A+self.B)/2,(self.A+self.C)/2,(self.A+self.D)/2
+#     def geametrik(self):
+#         return math.sqrt(self.A*self.B),math.sqrt(self.A*self.C),math.sqrt(self.A*self.D)
+# obj1 = Son(2,3,4,5)
+# print(obj1.arifmetik())
+# print(obj1.geametrik())
+
+4 # def Triangle(a):
+#     return math.sqrt(3)/4*a**2
+# print(Triangle(int(input())))
+
+5 # def RectPS(a,b):
+#     return f"Yuzasi: {a*b}\nPerimetri: {(a+b)*2}"
+# print(RectPS(a=int(input()),b=int(input())))
+
+20 # def TriangleP(a,b):
+#     c = math.sqrt(a**2+b**2)
+#     return a+b+c
+# print(TriangeP(int(input()),int(input())))
+
+21 # def SumRange(a,b,c):
+#     s = 0
+#     d = 0
+#     if c>b and b>a:
+#         for i in range(a+1,b):
+#             s += i
+#         for k in range(b+1,c):
+#             d += k
+#         return s+d
+# print(SumRange(int(input()),int(input()),int(input())))
+
+22 # def Calc(a,b,op):
+#     if op == 1:
+#         return a-b
+#     elif op == 2:
+#         return a*b
+#     elif op == 3:
+#         return a/b
+#     else:
+#         return a+b
+# print(Calc(int(input()),int(input()),int(input())))
+
+23 # def Quarter(x,y):
+#     if x!=0 and y!=0:
+#         if x>0 and y>0:
+#             return "Birinch choraokda yotadi"
+#         elif x<0 and y>0:
+#             return "Ikkinchi chorakda yotadi"
+#         elif x<0 and y<0:
+#             return "Uchinchi chorakda yotadi"
+#         elif x>0 and y<0:
+#             return "Turtinchi chorakda yotadi"
+# print(Quarter(int(input()),int(input())))
+
+24 # def Even(k):
+#     if k%2==0:
+#         return True
+#     elif k%2==1:
+#         return False
+# print(Even(int(input())))
+
+25 # def IsSquare(k):
+#     return math.log(k,math.sqrt(k))==2
+# print(IsSquare(int(input())))
+
+26 # def IsPower(k):
+#     a =  math.log(k,5)
+#     return 5**round(a)==k
+# print(IsPower(int(input())))
+
+27 # def IsPowerN(k,n):
+#     a =  math.log(n,k)
+#     return k**round(a)==n
+# print(IsPowerN(int(input()),int(input())))
+
+28 # def IsPrime(n):
+#     return n%2==1
+# print(IsPrime(int(input())))
+
+29 # def DigitCount(k):
+#     s = 0
+#     for i in k:
+#         if i.isdigit():
+#             s+=1
+#     return s
+# print(DigitCount(input()))
+
+
+
+
+
+
+
+
+
+
+
+32 # def DegToRad(d):
+#     return d*math.pi/180
+# print(DegToRad(int(input())))
+
+33 # def RadToDeg(r):
+#     return r*180/math.pi
+# print(RadToDeg(int(input())))
+
+34 # def Fact(n):
+#     s = 1
+#     for i in range(1,n+1):
+#         s = s * i
+#     return s
+# print(Fact(int(input())))
+
+35 # def Fact2(n):
+#     s = 1
+#     d = 1
+#     for i in range(1,n+1):
+#         s = s * i
+#     for k in range(1,s+1):
+#         d = d * k
+#     return d
+# print(Fact2(int(input())))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
