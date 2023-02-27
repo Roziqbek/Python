77e658a19ec16af76d7750f4f42416f6c4b353c9
Roziqbek Donobekovich <roziqbek33@gmail.com>
2023-02-27 18:33:13 +0500

    Update _Begin_.py

--- _Begin_.py
+++ _Begin_.py
@@ -73,3 +73,84 @@ import math
 #     S3 = math.pi*(r1-r2)
 #     return S1,S2,S3
 # print(Aylana(int(input()),int(input())))
+
+14 # def AylanaRS(l):
+#   R = l/2*math.pi
+#   S = math.pi*R
+#   return R,S
+# print(AylanaRS(int(input())))
+
+15 # def AylanadR(s):
+#   d = 2*math.sqrt(s/math.pi)
+#   r = d/2
+#   return d,r
+# print(AylanadR(float(input())))
+
+16 # def SonUqi(x1,x2):
+#   return abs(x1 - x2)
+# print(SonUqi(float(input()),float(input())))
+
+17 # def SonUqi(a,b,c):
+#   ac = abs(a - c)
+#   bc = abs(b - c)
+#   return ac,bc
+# print(SonUqi(float(input()),float(input()),float(input())))
+
+18 # def SonUqi(a,b,c):
+#   if a>c>b or b>c>a:
+#     ac = abs(a - c)
+#     bc = abs(b - c)
+#     return ac,bc
+# print(SonUqi(float(input()),float(input()),float(input())))
+
+19 # def ToriTurburchakPS(x1,y1,x2,y2):
+#   x = abs(x1-x2)
+#   y = abs(y1-y2)
+#   P = 2*(x+y)
+#   S = x*y
+#   return P,S
+# print(ToriTurburchakPS(float(input()),float(input()),float(input()),float(input())))
+
+20 # def KordinataUqi(x1,y1,x2,y2):
+#   c = math.sqrt((x2-x1)**2+(y2-y1)**2)
+#   return c
+# print(KordinataUqi(float(input()),float(input()),float(input()),float(input())))
+
+21 # def UchburchakSP(x1,y1,x2,y2,x3,y3):
+#   a = math.sqrt((x1-x2)**2+(y1-y2)**2)
+#   b = math.sqrt((x1-x3)**2+(y1-y3)**2)
+#   c = math.sqrt((x2-x1)**2+(y2-y1)**2)
+#   p = (a+b+c)/2
+#   S = math.sqrt(p(p-a)(p-b)(p-c))
+#   P = a+b+c
+# print(UchburchakSP(float(input()),float(input()),float(input()),float(input()),float(input()),float(input())))
+
+22 # def QiymatAlmashtirishAB(a,b):
+#   son = a
+#   a = b
+#   b = son
+#   return a,b
+# print(QiymatAlmashtirish(float(input()),float(input())))
+
+23 # def QiymatAlmashtirishABC(a,b,c):
+#   son = a
+#   son1 = b
+#   son2 = c
+#   a = son1
+#   b = son2
+#   c = son
+#   return a,b,c
+# print(QiymatAlmashtirishABC(float(input()),float(input()),float(input())))
+
+24 # def QiymatAlmashtirishABC(a,b,c):
+#   son = a
+#   son1 = b
+#   son2 = c
+#   a = son2
+#   b = son
+#   c = son1
+#   return a,b,c
+# print(QiymatAlmashtirishABC(float(input()),float(input()),float(input())))
+
+
+
