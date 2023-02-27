12fdd46bde761f1353776f38018bb0b3785661c5
Roziqbek Donobekovich <roziqbek33@gmail.com>
2023-02-27 16:57:10 +0500

    Update _Begin_.py

--- _Begin_.py
+++ _Begin_.py
@@ -1,7 +1,71 @@
+import math
+
 1 # def KvadratP(a):
 #   P = 4*a
 #   return P
 # print(KvadratP(int(input())))
+
+2 # def KvadratS(a):
+#   s = a**2
+#   return s
+# print(KvadratS(int(input())))
+
+3 # def TurtbuchakPS(a,b):
+#   P = (a+b)*2
+#   S = a*b
+#   return S,P
+# print(TurtbuchakPS(int(input()),int(input())))
+
+4 # def AylanaL(d):
+#   L = math.pi * d
+#   return L
+# print(AylanaL(int(input())))
+
+5 # def KubVS(a,b):
+#   V = a**3
+#   S = 6*(a**2)
+#   return V,S
+# print(KubVS(int(input()),int(input())))
+
+6 # def ParalelogramVS(a,b,c):
+#   V = a*b*c
+#   S = (a*b+a*c+b*c)*2
+#   return V,S
+# print(ParalelogramVS(int(input()),int(input()),int(input())))
+
+7 # def DoiraLS(r):
+#   L = 2*r*math.pi
+#   S = math.pi*(r**2)
+#   return L,S
+# print(DoiraLS(int(input())))
+
+8 # def Arifmetic(a,b):
+#   c = (a+b)/2
+#   return c
+# print(Arifmetik(int(input()),int(input())))
+
+9 # def Geometric(a,b):
+#   if a>0 and b>0:
+#     c = math.sqrt(a*b)
+#     return c
+# print(Geometric(int(input()),int(input())))
+
+10 # def Son(a,b):
+#   if a!=0 and b!=0:
+#     return a+b,a*b,a**2,b**2
+# print(Son(int(input()),int(input())))
+
+11 # def Son(a,b):
+#   if a!=0 and b!=0:
+#     return a+b,a*b,abs(a),abs(b)
+# print(Son(int(input()),int(input())))
+
+12 # def Turtburchak(a,b):
+#   c = math.sqrt(a**2+b**2)
+#   P = a+b+c
+#   return c,P
+# print(Turtburchak(int(input()),int(input())))
+
 
 
 
