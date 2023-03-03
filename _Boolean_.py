6d18ceba6875758a4127b8a33fd11a6e757c3e09
Roziqbek Donobekovich <roziqbek33@gmail.com>
2023-03-03 16:52:29 +0500

    Create _Boolean_

---
+++ _Boolean_.py
@@ -0,0 +1,256 @@
+import math
+
+1 # def new_function():
+#   a = int(input())
+#   return (a>0)
+# print(new_function())
+
+2 # def new_function():
+#   a = int(input())
+#   return (a%2!=0)
+# print(new_function())
+
+3 # def new_function():
+#   a = int(input())
+#   return (a%2==0)
+# print(new_function())
+
+4 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   return (a>2 and b<=3)
+# print(new_function())
+
+5 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   return (a>=0 or b<-2)
+# print(new_function())
+
+6 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   c = int(input())
+#   return (a<=b<=c)
+# print(new_function())
+
+7 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   c = int(input())
+#   return (a<b<c)
+# print(new_function())
+
+8 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   return (a%2!=0 and b%2!=0)
+# print(new_function())
+
+9 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   return (a%2!=0 or b%2!=0)
+# print(new_function())
+
+10 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   return ((a%2!=0 and b%2==0) or (a%2==0 and b%2!=0))
+# print(new_function())
+
+11 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   return ((a%2==0 and b%2==0) or (a%2!=0 and b%2!=0))
+# print(new_function())
+
+12 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   return (a>0 and b>0)
+# print(new_function())
+
+13 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   return (a>0 or b>0)
+# print(new_function())
+
+14 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   return ((a>0 and b<0) or (a<0 and b>0))
+# print(new_function())
+
+15 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   return (a>0 and b>0)
+# print(new_function())
+
+16 # def new_function():
+#   a = int(input())
+#   return (a%2==0 and a>=10)
+# print(new_function())
+
+17 # def new_function():
+#   a = int(input())
+#   return (a%2!=0 and a>=100)
+# print(new_function())
+
+18 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   c = int(input())
+#   return (a==b or a==c or b==c)
+# print(new_function())
+
+19 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   c = int(input())
+#   return ((a==-b) or (a==-c) or (b==-c))
+# print(new_function())
+
+20 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   c = int(input())
+#   return (a!=b and b!=c and a!=c)
+# print(new_function())
+
+21 # def new_function():
+#   a = int(input())
+#   return (100<=a<1000 and a//100+2==a//10%10+1==a%10)
+# print(new_function())
+
+22 # def new_function():
+#   a = int(input())
+#   return (100<=a<1000 and (a//100+2==a//10%10+1==a%10 or a//100-2==a//10%10-1==a%10))
+# print(new_function())
+
+23 # def new_function():
+#   a = int(input())
+#   return (100<=a<1000 and a//100==a//10%10==a%10)
+# print(new_function())
+
+24 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   c = int(input())
+#   D = b**2-4*a*c
+#   return (a!=0 and b**2-4*a*c>0)
+# print(new_function())
+
+25 # def new_function():
+#   x = int(input())
+#   y = int(input())
+#   return (x<0 and y>0)
+# print(new_function())
+
+26 # def new_function():
+#   x = int(input())
+#   y = int(input())
+#   return (x>0 and y<0)
+# print(new_function())
+
+27 # def new_function():
+#   x = int(input())
+#   y = int(input())
+#   return ((x<0 and y>0) or (x<0 and y<0))
+# print(new_function())
+
+28 # def new_function():
+#   x = int(input())
+#   y = int(input())
+#   return ((x>0 and y>0) or (x<0 and y<0))
+# print(new_function())
+
+
+
+
+
+
+
+30 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   c = int(input())
+#   return (a==b==c)
+# print(new_function())
+
+31 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   c = int(input())
+#   return ((a==b or b==c or a==c))
+# print(new_function())
+
+32 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   c = int(input())
+#   return (a**2+b**2==c**2 or a**2+c**2==b**2 or b**2+c**2==a**2)
+# print(new_function())
+
+33 # def new_function():
+#   a = int(input())
+#   b = int(input())
+#   c = int(input())
+#   return (a+b>c or a+c>b or b+c>a)
+# print(new_function())
+
+34 # def new_function():
+#   x = int(input())
+#   y = int(input())
+#   return (x,yand (x+y)%2!=0)
+# print(new_function())
+
+35 # def new_function():
+#   x = int(input())
+#   y = int(input())
+#   x1 = int(input())
+#   y1 = int(input())
+#   return (((x+y)%2!=0 and (x1+y1)%2!=0) or ((x+y)%2==0 and (x1+y1)%2==0))
+# print(new_function())
+
+36 # def new_function():
+#   x1 = int(input())
+#   y1 = int(input())
+#   x2 = int(input())
+#   y2 = int(input())
+#   return (((x1+y1)%2==0 and (x2+y2)%2==0) or ((x1+y1)%2!=0 and (x2+y2)%2!=0))
+# print(new_function())
+
+37 # def new_function():
+#   x = int(input())
+#   y = int(input())
+#   x1 = int(input())
+#   y1 = int(input())
+#   return ((x==x1-1 and y==y1) or (x==x1-1 and y==y1-1) or (x==x1+1 and y==y1-1) or (x==x1+1 and y==y1) or (x==x1-1 and y==y1+1) or (x==x1 and y==y1+1) or (x==x1+1 and y==y1+1) or (x==x1 and y==y1-1))
+# print(new_function())
+
+38 # def new_function():
+#   x = int(input())
+#   y = int(input())
+#   x1 = int(input())
+#   y1 = int(input())
+#   return (x1-x==y1-y)
+# print(new_function())
+
+39 # def new_function():
+#   x1 = int(input())
+#   y1 = int(input())
+#   x2 = int(input())
+#   y2 = int(input())
+#   return (((x1+y1)%2==0 and (x2+y2)%2==0) or ((x1+y1)%2!=0 and (x2+y2)%2!=0) or (x1==x2) or (y1==y2))
+# print(new_function())
+
+40 # def new_function():
+#   x = int(input())
+#   y = int(input())
+#   x1 = int(input())
+#   y1 = int(input())
+#   return ((x1-x)**2+(y1-y)**2==5)
+# print(new_function())
---
+++ _Integer_.py
@@ -0,0 +1,134 @@
+import math
+
+1 # def new_fun():
+#   L = float(input())
+#   return L//100
+# print(new_fun())
+
+2 # def new_function():
+#   M = float(input())
+#   return M//1000
+# print(new_functon())
+
+3 # def new_function():
+#   B = float(input())
+#   return B//1024
+# print(new_functon())
+
+4 # def new_function():
+#   A = float(input())
+#   B = float(input())
+#   if A>B:
+#       return A//B
+# print(new_functon())
+
+5 # def new_function():
+#   A = float(input())
+#   B = float(input())
+#   if A>B:
+#       return f'"butun qismi:",{A//B} /n "qoldiq:",{A%B}'
+# print(new_functon())
+
+6 # def new_function():
+#   a = int(input())
+#   if 10<=a<100:
+#       return f'{a//10} /n {a%10}'
+# print(new_functon())
+
+7 # def new_function():
+#   a = int(input())
+#   if 10<=a<100:
+#       return a//10+a%10
+# print(new_functon())
+
+8 # def new_function():
+#   a = int(input())
+#   son = a//10
+#   son2 = a%10
+#   return (str(son2)+str(son))
+# print(new_functon())
+
+9 # def new_function():
+#   a = int(input())
+#   return (a//100)
+# print(new_functon())
+
+10 # def new_function():
+#   a = int(input())
+#   if 100<=a<1000:
+#       return (f'{a//100} /n {a//10%10}')
+# print(new_functon())
+
+11 # def new_function():
+#   a = int(input())
+#   if 100<=a<1000:
+#       return (a//100+a//10%10+a%10)
+# print(new_functon())
+
+12 # def new_function():
+#   a = int(input())
+#   if 100<=a<1000:
+#       return (str(a%10)+str(a//10%10)+str(a//100))
+# print(new_functon())
+
+13 # def new_function():
+#   b = int(input())
+#   if 100<=b<1000:
+#       return (str(b//10%10)+str(b%10)+str(b//100))
+# print(new_functon())
+
+14 # def new_function():
+#   s = int(input())
+#   if 100<=s<1000:
+#       return (str(s%10)+str(s//100)+str(s//10%10))
+# print(new_functon())
+
+15 # def new_function():
+#   a = int(input())
+#   if 100<=a<1000:
+#       return (str(a//10%10)+str(a//100)+str(a%10))
+# print(new_functon())
+
+16 # def new_function():
+#   b = int(input())
+#   if 100<=b<1000:
+#       return (str(b//100)+str(b%10)+str(b//10%10))
+# print(new_functon())
+
+17 # def new_function():
+#   b = int(input())
+#   if 999<b:
+#       return (b//100%10)
+# print(new_functon())
+
+18 # def new_function():
+#   b = int(input())
+#   if 999<b:
+#       return (b//1000)
+# print(new_functon())
+
+19 # def new_function():
+#   c = int(input())
+#   return (c/60,"minut")
+# print(new_functon())
+
+20 # def new_function():
+#   c = int(input())
+#   return (c/3600,"soat")
+# print(new_functon())
+
+21 # def new_function():
+#   c = int(input())
+#   return (f"{c//60}minuttu {c%60}sekunt")
+# print(new_functon())
+
+22 # def new_function():
+#   c = int(input())
+#   return (f"{c//3600}soat {c%3600}sekunt")
+# print(new_functon())
+
+23 # def new_function():
+#   c = int(input())
+#   return (f"{c//3600}soatu {c%3600//60}minut {c%3600%60}sekunt")
+# print(new_functon())
+
