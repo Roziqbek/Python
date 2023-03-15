f1d3b9b9d7d01d06a1248ecd00902a734beb3a6b
Roziqbek Donobekovich <roziqbek33@gmail.com>
2023-03-15 16:24:36 +0500

    Update _For_.py

--- _For_.py
+++ _For_.py
@@ -1,3 +1,23 @@
+import math
+
+29 # a = int(input())
+# b = int(input())
+# for i in range(a,b+1):
+#   print(i)
+
+30 # n = int(input())
+# a = int(input())
+# b = int(input())
+# for i in range(a,b+1):
+#   print(1-math.sin(i))
+
+31 # def new_function(n):
+#   if n == 0:
+#     return 2
+#   else:
+#     return 2+1/new_function(n-1)
+# print(new_function(int(input())))
+
 32 # def new_function(k):
 #   if k==0:
 #     return 1
@@ -31,6 +51,26 @@
 #   son2 = continue1
 #   print(continue1)
 
+#######################################################
+
+35 # def new_function(k):
+#   if k == 1:
+#     return 1
+#   elif k == 2:
+#     return 2
+#   elif k == 3:
+#     return 3
+#   else:
+#     return new_function(k-1)+new_function(k-2)-2*(new_function(k-3))
+# print(new_function(int(input())))
+
+
+36 # k = int(input())
+# s = 0
+# for i in range(1,k+1):
+#   s += (i**k)
+# print(s)
+
 
 37 # n = int(input())
 # s = 0
@@ -46,5 +86,11 @@
 
 39 # n = int(input())
 # for i in range(n+1):
+#   for j in range(i):
+#     print(i)
+
+40 # a = int(input())
+# b = int(input())
+# for i in range(a+1,b):
 #   for j in range(i):
 #     print(i)
