5540febe15379c8f3039aff92e850af8bebd77b9
Roziqbek Donobekovich <roziqbek33@gmail.com>
2023-04-28 18:08:27 +0500

    first commit asdf agf

---
+++ _Array_.py
@@ -0,0 +1,50 @@
+1 # n = int(input())
+# list = []
+# for i in range(1,n+1):
+#   if i%2==1:
+#     list.append(i)
+# print(list)
+
+2 # n = int(input())
+# list = []
+# for i in range(0,n+1):
+#   list.append(2**i)
+# print(list)
+
+3 # a = int(input())
+# sonlar = [a]
+# d = 2
+# n = 4
+# for i in range(1,n):
+#   continue1 = a + d
+#   a = continue1
+#   sonlar.append(continue1)
+# print(sonlar)
+
+4 # b = int(input())
+# sonlar = [b]
+# q = 2
+# n = 4
+# for i in range(1,n):
+#   continue1 = b * q
+#   b = continue1
+#   sonlar.append(continue1)
+# print(sonlar)
+
+
+########################################################## N gacha bulgan arifmetik progressiyaning yigindisi ########################################################################
+# d = int(input())
+# a = 2
+# sonlar = [a]
+# n = 4
+# for i in range(1,n):
+#   continue1 = a + d
+#   a = continue1
+#   sonlar.append(continue1)
+# S = 0
+# for j in sonlar:
+#   S = j + S
+# print(sonlar)
+# print(S)
+
+
